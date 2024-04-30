2fc5b26186d93d19123fbbef456fa98969d28a89
Andrey.Bertoletti <andrey.bertolettiteste@gmail.com>
2024-04-30 08:23:22 -0300

    first commit

---
+++ AMST4_Carousel.MVC/Controllers/CategoryController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AMST4.Carousel.MVC.Context;
+
+namespace AMST4_Carousel.MVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly DataContext _DataContext;
+        public CategoryController(DataContext DataContext)
+        {
+            _DataContext = DataContext;
+        }
+        public IActionResult CategoryList()
+        {
+            return View();
+        }
+    }
+}
